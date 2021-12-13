7fc75809074f797c58519a7c9bec92205dfd69fe
Abin Michael <abinvalliyil@gmail.com>
2021-12-13 08:28:27 +0530

    create_crud_operations

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from types import prepare_class
+from fastapi import FastAPI,status,HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from typing import List, Optional
+from starlette.status import HTTP_200_OK
+from database import SessionLocal
+import models
 
 app = FastAPI()
-
+ 
 
 class Item(BaseModel):
     id:int
@@ -11,17 +15,64 @@ class Item(BaseModel):
     description:str
     price:int
     on_offer:bool
+     
+    class Config:
+         orm_mode = True
+          
+
+db = SessionLocal()
+
+@app.get("/items",response_model=List[Item],status_code=200)
+def get_all_items():
+    items = db.query(models.Item).all()
+    return items
+
+@app.get('/item_id/{item_id}',response_model=Item,status_code=HTTP_200_OK)
+def get_item_id(item_id:int):
+    item = db.query(models.Item).filter(models.Item.id==item_id).first()
+    return item
 
 
-@app.put('/item{item_id}')
-def update_item(item_id:int,item:Item):
-    return{'name':item.name}
+@app.post('/items',response_model=Item,status_code=status.HTTP_201_CREATED)
+
+def create_an_item(item:Item):
+    new_item = models.Item(
+        name =item.name,
+        price = item.price,
+        description = item.description,
+        on_offer = item.on_offer
+    )
+    
+    db_item = db.query(models.Item).filter(models.Item.name == item.name).first()
+
+    if db_item is not  None:
+        raise HTTPException(status_code=400,detail="Item already exists")
 
 
-@app.get('/')
-def index():
-    return{'message':'hello'}
+    db.add(new_item)
+    db.commit()
 
-@app.get('/greet/{name}')
-def item_name(name:str):
-    return{'greeting':f"hello {  name}"}
+    return new_item
+
+@app.put('/item/{item_id}',response_model=Item,status_code=HTTP_200_OK)
+def update_an_item(item_id:int,item:Item):
+    item_to_update = db.query(models.Item).filter(models.Item.id==item_id).first()
+    item_to_update.name=item.name
+    item_to_update.price =item.price
+    item_to_update.description =item.description
+    item_to_update.on_offer=item.on_offer
+
+    db.commit()
+
+    return item_to_update
+
+@app.delete('/item/{item_id}')
+def delete_item(item_id:int):
+    item_to_delete = db.query(models.Item).filter(models.Item.id==item_id).first()
+
+    if item_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Resource not found")
+
+    db.delete(item_to_delete)
+    db.commit()
+    return item_to_delete
